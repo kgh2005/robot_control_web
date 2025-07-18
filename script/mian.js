9b53182467f978f251e0cb66718406c0f71debc0
kgh <leokim0503@kw.ac.kr>
2025-07-18 15:32:13 +0900

    init

--- script/mian.js
+++ script/mian.js
@@ -154,6 +154,148 @@ function updateYawOnCanvas2(yawDeg) {
 }
 // ============================================================
 
+//============================================================
+// 3번 로봇 관련 코드
+// ============================================================
+// ROS 토픽 구독 설정
+const robot3Listener = new ROSLIB.Topic({
+    ros,
+    name: "/robot3receiver",
+    messageType: "humanoid_interfaces/Robot3receiverMsg",
+});
+
+let robotX_3 = 275;
+let robotY_3 = 200;
+let ballX_3 = 0;
+let ballY_3 = 0;
+let currentYaw_3 = 0;
+
+robot3Listener.subscribe((message) => {
+    const pitch = message.pitch;
+    const roll = message.roll;
+    const yaw = message.yaw;
+
+    robotX_3 = message.robot_x * 0.5;
+    robotY_3 = message.robot_y * 0.5;
+    ballX_3 = message.ball_x * 0.5;
+    ballY_3 = message.ball_y * 0.5;
+
+    draw3();
+
+    document.getElementById("orientation3").innerText =
+        `Roll: ${roll.toFixed(1)}°, Pitch: ${pitch.toFixed(1)}°, Yaw: ${yaw.toFixed(1)}°`;
+
+    updateYawDial3(yaw);
+    updateYawOnCanvas3(yaw + 90);
+});
+
+function updateYawDial3(yaw) {
+    const pointer = document.getElementById("yaw-pointer3");
+    const angle = yaw * (-1);
+    pointer.style.transform = `rotate(${angle}deg)`;
+
+    currentYaw_3 = yaw;
+    draw3();
+}
+
+function sendTestMessage3(setValue) {
+    const publisher = new ROSLIB.Topic({
+        ros: ros,
+        name: "/robot3sender",
+        messageType: "humanoid_interfaces/Robot3senderMsg"
+    });
+
+    const msg = new ROSLIB.Message({ set: setValue });
+    publisher.publish(msg);
+}
+
+const arrowLength3 = 15;
+
+// 캔버스 관련 변수
+let ctx3, img3, canvas3
+
+function draw3() {
+    drawRobot(ctx3, img3, robotX_3, robotY_3, currentYaw_3, ballX_3, ballY_3, arrowLength3);
+}
+
+// 외부에서 yaw 업데이트 시 draw() 호출
+function updateYawOnCanvas3(yawDeg) {
+    currentYaw_3 = yawDeg;
+    draw3();
+}
+// ============================================================
+
+//============================================================
+// 3번 로봇 관련 코드
+// ============================================================
+// ROS 토픽 구독 설정
+const robot4Listener = new ROSLIB.Topic({
+    ros,
+    name: "/robot4receiver",
+    messageType: "humanoid_interfaces/Robot4receiverMsg",
+});
+
+let robotX_4 = 275;
+let robotY_4 = 200;
+let ballX_4 = 0;
+let ballY_4 = 0;
+let currentYaw_4 = 0;
+
+robot4Listener.subscribe((message) => {
+    const pitch = message.pitch;
+    const roll = message.roll;
+    const yaw = message.yaw;
+
+    robotX_4 = message.robot_x * 0.5;
+    robotY_4 = message.robot_y * 0.5;
+    ballX_4 = message.ball_x * 0.5;
+    ballY_4 = message.ball_y * 0.5;
+
+    draw4();
+
+    document.getElementById("orientation4").innerText =
+        `Roll: ${roll.toFixed(1)}°, Pitch: ${pitch.toFixed(1)}°, Yaw: ${yaw.toFixed(1)}°`;
+
+    updateYawDial4(yaw);
+    updateYawOnCanvas4(yaw + 90);
+});
+
+function updateYawDial4(yaw) {
+    const pointer = document.getElementById("yaw-pointer4");
+    const angle = yaw * (-1);
+    pointer.style.transform = `rotate(${angle}deg)`;
+
+    currentYaw_4 = yaw;
+    draw4();
+}
+
+function sendTestMessage4(setValue) {
+    const publisher = new ROSLIB.Topic({
+        ros: ros,
+        name: "/robot4sender",
+        messageType: "humanoid_interfaces/Robot4senderMsg"
+    });
+
+    const msg = new ROSLIB.Message({ set: setValue });
+    publisher.publish(msg);
+}
+
+const arrowLength4 = 15;
+
+// 캔버스 관련 변수
+let ctx4, img4, canvas4
+
+function draw4() {
+    drawRobot(ctx4, img4, robotX_4, robotY_4, currentYaw_4, ballX_4, ballY_4, arrowLength4);
+}
+
+// 외부에서 yaw 업데이트 시 draw() 호출
+function updateYawOnCanvas4(yawDeg) {
+    currentYaw_4 = yawDeg;
+    draw4();
+}
+// ============================================================
+
 // ============================================================
 // 공통 함수
 // ============================================================
@@ -199,11 +341,22 @@ window.onload = function () {
     canvas2 = document.getElementById("fieldCanvas2");
     ctx2 = canvas2.getContext("2d");
 
-    if (img1.complete) {
+    img3 = document.getElementById("fieldImage3");
+    canvas3 = document.getElementById("fieldCanvas3");
+    ctx3 = canvas3.getContext("2d");
+
+    img4 = document.getElementById("fieldImage4");
+    canvas4 = document.getElementById("fieldCanvas4");
+    ctx4 = canvas4.getContext("2d");
+
+    if (img1.complete) 
+    {
         canvas1.width = img1.width;
         canvas1.height = img1.height;
         draw1();
-    } else {
+    } 
+    else 
+    {
         img1.onload = () => {
             canvas1.width = img1.width;
             canvas1.height = img1.height;
@@ -211,15 +364,48 @@ window.onload = function () {
         };
     }
 
-    if (img2.complete) {
+    if (img2.complete) 
+    {
         canvas2.width = img2.width;
         canvas2.height = img2.height;
         draw2();
-    } else {
+    }
+    else 
+    {
         img2.onload = () => {
             canvas2.width = img2.width;
             canvas2.height = img2.height;
             draw2();
+        };
+    }
+
+    if (img3.complete) 
+    {
+        canvas3.width = img3.width;
+        canvas3.height = img3.height;
+        draw3();
+    } 
+    else 
+    {
+        img3.onload = () => {
+            canvas3.width = img3.width;
+            canvas3.height = img3.height;
+            draw3();
+        };
+    }
+
+    if (img4.complete) 
+    {
+        canvas4.width = img4.width;
+        canvas4.height = img4.height;
+        draw4();
+    } 
+    else 
+    {
+        img4.onload = () => {
+            canvas4.width = img4.width;
+            canvas4.height = img4.height;
+            draw4();
         };
     }
 };
